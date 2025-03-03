a5ecd45debdad42f0cc925ee6d714eda14753568
AnnaNowak27 <nowak.anna2006@gmail.com>
2025-03-03 16:36:27 +0100

    Koniec zajęć

---
+++ Zadanie 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący boki prostokąta i wypisujący na ekranie jego pole i obwód.
+            double a, b;
+            Console.WriteLine("Wpisz a:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Wpisz b:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Obwód={0}", a+a+b+b);
+            Console.WriteLine("Pole={0}", a*b);
+            Console.ReadKey(true);//pauza
+
+        }
+    }
+}
---
+++ Zadanie2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie;
+            Console.WriteLine("Wpisz imie");
+            imie = Console.ReadLine();
+            Console.WriteLine("Witaj {0}!",imie);
+            Console.ReadKey(true);//pauza
+
+        }
+    }
+}
