6220576488fa1e620a9ec3138f57697e146e88a4
Anna Nowak <nowak.anna2006@gmail.com>
2025-03-07 14:42:07 +0100

    Koniec zajęć

---
+++ Zad 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Napisać program wczytujący liczbę całkowitą i wypisujący na ekranie informację czy jest to liczba parzysta, czy też nieparzysta.//
+            int a;
+            Console.WriteLine("Podaj liczbe calkowita");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0) 
+            Console.WriteLine("{0} to jest liczba parzysta", a);
+            else
+                Console.WriteLine("{0} to jest liczba nieparzysta", a);
+
+        }
+    }
+}
---
+++ Zad 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Napisać program wczytujący liczbę całkowitą i wypisujący informację, czy wczytana liczba jest dodatnia, ujemna, czy też równa zero.//
+            int a;
+            Console.WriteLine("Podaj liczbe calkowita");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0)
+                Console.WriteLine("{0} to jest liczba dodatnia", a);
+            else if (a < 0)
+                Console.WriteLine("{0} to jest liczba ujemna", a);
+            else
+                Console.WriteLine("{0} to jest liczba zero", a);
+        }
+    }
+}
