e52a686a980364783ee2d235239da60ad24f4834
AnnaNowak27 <a.nowak.024@studms.ug.edu.pl>
2025-03-17 16:04:44 +0100

    Zadanie 1, 2 (tylko polecenie)

---
+++ Zad_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący wartość liczby naturalnej n (n>=0), obliczający wartość 2^n (2 do
+            potęgi n) i wypisujący wynik na ekranie w formacie(np.dla n = 10) : 2 ^ 10 = 1024.Do obliczenia
+            wartości wyrażenia użyj pętli for.//
+                    
+        }
+    }
+}
