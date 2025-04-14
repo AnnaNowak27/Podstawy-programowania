de2f3e3ef52b7fe8ba26f80f66507025fc178c1a
AnnaNowak27 <a.nowak.024@studms.ug.edu.pl>
2025-04-14 13:48:10 +0200

    dokończyć

---
+++ Metody_zad_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody_zad_1
+{
+    class Program
+    {
+        static int suma(int a, int b)
+        {
+        return a + b;
+        }
+           
+        static void Main(string[] args)
+        {
+            //Napisz funkcję o nazwie suma wyznaczającą sumę jej dwóch argumentów będących liczbami
+            //całkowitymi.Napisz program wykorzystujący funkcję suma.
+            Console.WriteLine("2+5={0}", suma(2, 5));
+            Console.ReadKey(true);
+
+        }
+    }
+}
