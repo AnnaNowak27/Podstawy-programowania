99216dde168a7a5fbcf0c802faae972c51c05620
AnnaNowak27 <a.nowak.024@studms.ug.edu.pl>
2025-04-14 08:59:22 +0200

    aaaaa

---
+++ Zad_14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i;
+            for (i=0; i<=10; i++)
+            {
+                if (i > 0)
+                    Console.WriteLine("Liczba dodatnia.", i);
+                else if (i > 0)
+                    Console.WriteLine("liczba ujemna.", i);
+                else
+                    Console.WriteLine("To jest zero.", i);
+
+            }Console.ReadKey(true);
+        }
+    }
+}
