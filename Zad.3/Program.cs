1d6e4a394bc529cbf8d04bd316c6b62b0d490f46
AnnaNowak27 <a.nowak.024@studms.ug.edu.pl>
2025-05-22 14:08:46 +0200

    hhhhh

--- Zad.3/Program.cs
+++ Zad.3/Program.cs
@@ -13,7 +13,7 @@ namespace Zad._3
             //Napisać program wczytujący ciąg n (1<=n<=1000) liczb całkowitych, sortujący je niemalejąco i wypisujący posortowane liczby na ekranie.
             int[] tab = new int[1000];
             int i, j;
-            int n, tmp;
+            int n, t;
             Console.Write("Podaj ilość elementów tablicy n<=1000:");
             n = Convert.ToInt32(Console.ReadLine());
             for (i = 0; i < n; i++)
@@ -24,7 +24,7 @@ namespace Zad._3
             for (i=0; i<n; i++)
                 for(j=i+1; j<n; j++)
                     if (tab[i] > tab[j])
-                    { tmp = tab[i]; tab[i] = tab[j]; tab[j] = tmp; }
+                    { t = tab[i]; tab[i] = tab[j]; tab[j] = t; }
             Console.Write("Elementy tablicy:");
             for (i=0; i<n; i++)
             {
---
+++ Zad.7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Napisać program wczytujący ciąg n (1<=n<=1000) liczb całkowitych i wypisujący na ekranie
+            //najmniejszą i największą wczytaną liczbę.
+            int[] liczby = new int[1000];
+            int i, n;
+            Console.Write("Podaj ilość elementów n<=1000:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Podaj liczby[{0}]= ", i);
+                liczby[i] = Convert.ToInt32(Console.ReadLine()); // Wczytaj każdą liczbę
+            }
+            int min = liczby[0];
+            int max = liczby[0];
+
+            foreach (int liczba in liczby)
+            {
+                if (liczba < min) min = liczba;
+                if (liczba > max) max = liczba;
+            }
+
+            Console.WriteLine("Najmniejsza: ", min);
+            Console.WriteLine("Największa: ",  max);
+        }
+    }
+}
+    
+
